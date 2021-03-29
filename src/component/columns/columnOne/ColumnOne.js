55040c420b63af870389a13f5d386c71ce92277a
windoxas <xurshidbekjon@mail.ru>
2021-03-29 16:56:50 +0500

    change Some File

--- src/component/columns/columnOne/ColumnOne.js
+++ src/component/columns/columnOne/ColumnOne.js
@@ -10,17 +10,38 @@ import './column_One.css'
 function ColumnOne(){
 
 
- const [retTime, setRetTime] = React.useState(localStorage.getItem('setTimers')) 
+let [countUp, setCountUp] = React.useState()
+let [countDown, setCountDown] = React.useState();
+
+let hour = 0;
+let minut = 0;
+let second =0;
 
 
-localStorage.setItem('setTimers', retTime)
- setInterval(() => {
-     let times = moment().set({h:12, m:"00", s:"00"}).format('HH:mm:ss')
-    setRetTime(times)
- },1000)
 
- 
+React.useEffect(() => {
+    function countUpTime(){
+        let times = moment().set({h:hour, m:minut, s:second}).format('HH:mm:ss')
+        setCountUp(times)
+        second++
+    }
+    setInterval(countUpTime, 1000)
+},[])
 
+
+let hours = {
+    hourDown:5,
+    MinutDown:45,
+    SecondDown:0
+}
+React.useEffect(() => {
+    function countUpTime(){
+        let timesDown = moment().set({h:hours.hourDown, m:hours.MinutDown, s:hours.SecondDown}).format('HH:mm:ss')
+        setCountDown(timesDown)
+        hours.SecondDown--
+    }
+    setInterval(countUpTime, 1000)
+},[])
 
     return(
         <>
@@ -30,29 +51,29 @@ localStorage.setItem('setTimers', retTime)
                     <h1 className="train_number">2170</h1>
                   
                     <div className="train_drive_person">
-                       
+                    
                         <PersonOne mashFirst="Хидиров Джурабек" mashSecond="Сидиков Олим"/>
                         <PersonTwo/>
                         <PersonThree/>
-                        
+                    
                     </div>
                     <div className="team_change">
                         <div className="true_work">
                             <h3>На работе</h3>
-                            <p>Хидиров Джурабек <span>{retTime}</span></p>
-                            <p>Шаманов Фарух <span>{retTime}</span></p>
+                            <p>Хидиров Джурабек <span className="show_countUp">{countUp}</span></p>
+                            <p>Шаманов Фарух <span className="show_countUp">{countUp}</span></p>
                         </div>
                         <div className="false_work">
                             <h3>На отдыхе</h3>
-                            <p>Саидов Олим <span>{retTime}</span></p>
-                            <p>Хидиров Рауф <span>{retTime}</span></p>
+                            <p>Саидов Олим <span>{countDown}</span></p>
+                            <p>Хидиров Рауф <span>{countDown}</span></p>
                         </div>
                     </div>
                        
                 </div>
-
-                <DateTime/>
-
+          
+                    <DateTime/>
+              
         </div>
         
         </>
--- src/component/listPerson/list_compenents/Date_time.js
+++ src/component/listPerson/list_compenents/Date_time.js
@@ -7,8 +7,6 @@ function DateTime(){
     const [time, setTime] = React.useState(localStorage.getItem('getTime'))
     const [endData, setEndData] = React.useState(localStorage.getItem('endDate'))
     const [endTime, setEndTime] = React.useState(localStorage.getItem('endTime'))
-
-console.log("time", parseInt(time) - parseInt(endTime) );
 localStorage.setItem('getDate', date)
     function getData(){
         const NowDate = moment().format('YYYY-MM-DD')
@@ -23,23 +21,20 @@ localStorage.setItem('getTime', time)
 
 // END TIMES
 localStorage.setItem('endTime', endTime)
-function endTimes(){
+function endTimes(e){
         let minute = parseInt(time.slice(3,5))
-
         let getTimeSendEndTime =  moment().hours(parseInt(time)).minutes(minute).add(12, 'hours').format('HH:mm')
-        console.log(getTimeSendEndTime);
         return  setEndTime(getTimeSendEndTime)
     }
 
 localStorage.setItem('endDate', endData)
 function endDates(){
     let getDateSendEndTime =  moment().hours(parseInt(time)).add(12, 'hours').format('YYYY-MM-DD')
-    console.log(getDateSendEndTime);
     setEndData(getDateSendEndTime.toString())
 }
 
     setTimeout(() => {
-        endTimes()
+        
         endDates()
         
     }, 2000)
@@ -48,6 +43,12 @@ function sendToDb(){
         
          endTimes()
      }
+
+
+ function resualtTime (){
+
+ }
+
 // END TIMES END
     return(
             <div className="train_time">
@@ -64,9 +65,9 @@ function sendToDb(){
                 <div className="train_end">
                             <h4>Зак.Явка</h4>
                             <input value={endData} onChange={() => endDates()}  type="date"/>
-                            <input defaultValue={endTime} onChange={() => endTimes()}  type="time"/>
+                            <input defaultValue={endTime} onChange={(e) => endTimes(e)}  type="time"/>
                         </div>
-                        <button className="end_work">Закончить</button>
+                        <button onClick={() => resualtTime()} className="end_work">Закончить</button>
             </div>
         </div>
     )
