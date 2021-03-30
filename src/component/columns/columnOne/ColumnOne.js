339b003e46e58c9c9a995d69cd534cff9543bbdc
windoxas <xurshidbekjon@mail.ru>
2021-03-30 17:04:16 +0500

    add new file and change some file

--- src/component/columns/columnOne/ColumnOne.js
+++ src/component/columns/columnOne/ColumnOne.js
@@ -9,18 +9,19 @@ import './column_One.css'
 
 function ColumnOne(){
 
-
 let [countUp, setCountUp] = React.useState()
 let [countDown, setCountDown] = React.useState();
 
-let hour = 0;
-let minut = 0;
-let second =0;
+
 
 
 
 React.useEffect(() => {
+    let hour = 0;
+    let minut = 0;
+    let second = localStorage.getItem('countsUp')
     function countUpTime(){
+        localStorage.setItem('countsUp', second)
         let times = moment().set({h:hour, m:minut, s:second}).format('HH:mm:ss')
         setCountUp(times)
         second++
@@ -29,19 +30,25 @@ React.useEffect(() => {
 },[])
 
 
-let hours = {
-    hourDown:5,
-    MinutDown:45,
-    SecondDown:0
-}
 React.useEffect(() => {
+    let hours = {
+        hourDown:5,
+        MinutDown:45,
+        SecondDown:localStorage.getItem('countDown')
+    }
+    localStorage.setItem('minuteDown', hours.MinutDown)
     function countUpTime(){
+        localStorage.setItem('countDown', hours.SecondDown)
         let timesDown = moment().set({h:hours.hourDown, m:hours.MinutDown, s:hours.SecondDown}).format('HH:mm:ss')
         setCountDown(timesDown)
         hours.SecondDown--
     }
+
     setInterval(countUpTime, 1000)
 },[])
+
+
+
 
     return(
         <>
@@ -60,13 +67,13 @@ React.useEffect(() => {
                     <div className="team_change">
                         <div className="true_work">
                             <h3>На работе</h3>
-                            <p>Хидиров Джурабек <span className="show_countUp">{countUp}</span></p>
+                            <p >Хидиров Джурабек <span className="show_countUp">{countUp}</span></p>
                             <p>Шаманов Фарух <span className="show_countUp">{countUp}</span></p>
                         </div>
                         <div className="false_work">
                             <h3>На отдыхе</h3>
-                            <p>Саидов Олим <span>{countDown}</span></p>
-                            <p>Хидиров Рауф <span>{countDown}</span></p>
+                            <p style={countDown <  "00:00:01" ? {background:'yellow', color:'black'}: {background:'red'}}>Саидов Олим <span>{countDown}</span></p>
+                            <p style={countDown <  "00:00:01" ? {background:'yellow', color:'black'}: {background:'red'}}>Хидиров Рауф <span>{countDown}</span></p>
                         </div>
                     </div>
                        
--- src/component/columns/columnTwo/ColumnTwo.js
+++ src/component/columns/columnTwo/ColumnTwo.js
@@ -1,8 +1,4 @@
 import React from 'react'
-import PersonOne from '../../listPerson/list_compenents/PersonOne'
-import PersonTwo from '../../listPerson/list_compenents/PersonTwo'
-import PersonThree from '../../listPerson/list_compenents/PersonThree'
-// import DateTime from './list_compenents/Date_time'
 
 
 
@@ -11,21 +7,8 @@ function ColumnTwo(){
 
     return(
         <>
-        <div className="list_person">
-                <div className="train">
-                    <h1 className="train_number">8570</h1>
-                    <div className="train_drive_person">
-                       
-                        <PersonOne/>
-                        <PersonTwo/>
-                        <PersonThree/>
-                        
-                    </div>
-                       
-                </div>
 
-        </div>
-        
+            <h1>Hello</h1>
         </>
         )
 }
